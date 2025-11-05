55688c398e3d2dae7174a83f618930bec405e8a2
Patrick Boynton <youremail@yourdomain.com>
2025-11-05 13:27:53 -0500

    Added python dotenv

--- models/create_video.py
+++ models/create_video.py
@@ -4,7 +4,9 @@ import os, subprocess
 from models import Video
 from models.video_status import VideoStatus
 from db import db
+from dotenv import load_dotenv
 
+ip_address = os.getenv('IP_ADDRESS')
 
 def get_duration(file_name):
     result = subprocess.run(
@@ -52,7 +54,7 @@ def create_video():
 
         video = Video(
             title=os.path.splitext(file)[0],
-            url=f"http://192.168.1.156:5000/{file}",
+            url=f"http://{ip_address}:5000/{file}",
             image=None,
             duration=get_duration(path + file),
             uploaded_date=datetime.datetime.now(),
