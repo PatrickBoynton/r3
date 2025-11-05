f6c3112136c6d11bec4d5edbd6df8d45db72f66a
Patrick Boynton <youremail@yourdomain.com>
2025-11-05 13:37:37 -0500

    Added a data folder creation routine.

--- start.py
+++ start.py
@@ -1,9 +1,10 @@
 import socket
+import os
 
 #  Run this file first.
 r3vids = "/home/mymar101/r3vids/.env"
 r3vue = "/home/mymar101/r3vids/.env"
-
+data_folder = "/home/mymar101/r3vids/data"
 
 hostname = socket.gethostname()
 ip_address = socket.gethostbyname(hostname)
@@ -25,4 +26,7 @@ else:
     with open(r3vue, "w") as file:
         file.write(f"VITE_IP_ADDRESS=localhost")
 
-print(f'FINISHED ADDING IP ADDRESS {ip_address}')
+if not os.path.exists(data_folder):
+    os.makedirs(data_folder)
+
+print(f"FINISHED ADDING IP ADDRESS {ip_address}")
