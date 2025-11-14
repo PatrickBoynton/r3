75a0be85d22edd7dbcc8c1b9ac3a6b788ae22ad5
Patrick Boynton <youremail@yourdomain.com>
2025-11-14 12:09:46 -0500

    Fixed a url to the vue app.

--- start.py
+++ start.py
@@ -1,9 +1,9 @@
-import socket
 import os
+import socket
 
 #  Run this file first.
 r3vids = "/home/mymar101/r3vids/.env"
-r3vue = "/home/mymar101/r3vids/.env"
+r3vue = "/home/mymar101/r3vids/r3vue/.env"
 data_folder = "/home/mymar101/r3vids/data"
 
 hostname = socket.gethostname()
@@ -13,22 +13,17 @@ if "192" in ip_address:
     print(f"hostname: {hostname}")
     print(f"ip_address: {ip_address}")
 
-    with open(r3vids, "w") as file:
+    with open(r3vids, "w") as file, open(r3vue, "w") as file2:
         file.write(f"IP_ADDRESS={ip_address}")
+        file2.write(f"VITE_IP_ADDRESS={ip_address}")
 
-    with open(r3vue, "w") as file:
-        file.write(f"VITE_IP_ADDRESS={ip_address}")
 else:
     print("localhost")
-    with open(r3vids, "w") as file:
+    with open(r3vids, "w") as file, open(r3vue, "w") as file2:
         file.write(f"IP_ADDRESS=localhost")
-
-    with open(r3vue, "w") as file:
-        file.write(f"VITE_IP_ADDRESS=localhost")
+        file2.write(f"VITE_IP_ADDRESS=localhost")
 
 if not os.path.exists(data_folder):
     os.makedirs(data_folder)
 else:
     print("Data folder exists.")
-
-print(f"FINISHED ADDING IP ADDRESS {ip_address}")
