4f9c4832ab794828c87831daaf2f0a8e6d3c952a
Patrick Boynton <youremail@yourdomain.com>
2025-12-08 13:49:23 -0500

    Fixed a bug with the seconds.

--- r3vue/src/utils.ts
+++ r3vue/src/utils.ts
@@ -5,7 +5,7 @@ export const convertToPlayTime = (time: number) => {
     const dateObj = new Date(time * 1000)
     const hours = dateObj.getUTCHours()
     const minutes = dateObj.getUTCMinutes()
-    const seconds = dateObj.getUTCSeconds()
+    const seconds = dateObj.getUTCSeconds() + 1
     const timeString = `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`
 
     return timeString
