2e284bd0a72a7869149feb8d0732d4110f81fac9
Patrick Boynton <youremail@yourdomain.com>
2026-05-30 11:41:01 -0400

    Changed the try except.

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import threading
-
+import zoneinfo
+from datetime import datetime
 
 def set_interval(func, seconds, app):
     def func_wrapper():
@@ -7,9 +8,11 @@ def set_interval(func, seconds, app):
             with app.app_context():
                 func()
                 set_interval(func, seconds, app)
-        except:
-            print("Func cannot run.")
-            set_interval(func, seconds, app)
+        except Exception as e:
+            print(f"Func cannot run. {e}", flush=True)
+        finally:
+            print(f'Finish Scan at: {datetime.now(zoneinfo.ZoneInfo("America/New_York")).strftime("%-I:%M %p")}',
+                  flush=True)
 
     t = threading.Timer(seconds, func_wrapper)
 
